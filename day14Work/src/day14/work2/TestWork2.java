3773aa106bcd3dc904d9649d05e8d465aa281762
sang678 <tkdrmsdlzz@naver.com>
2014-07-22 20:32:46 +0900

    문제2번까지

--- day14Work/src/day14/work2/TestWork2.java
+++ day14Work/src/day14/work2/TestWork2.java
@@ -1,10 +1,32 @@
 package day14.work2;
 
+import java.util.StringTokenizer;
+
 public class TestWork2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String s = "J a v a  P r o g r a m";
+		
+		StringTokenizer st = new StringTokenizer(s, " ");
+		
+		char ch[] = new char[st.countTokens()];
+		for(int i=0; st.hasMoreTokens(); i++)
+			ch[i] = st.nextToken().charAt(0);
+		
+		
+		
+		
+		String s2 = s.valueOf(ch);
+		
+		s2 = s2.toUpperCase();
+		
+		System.out.println("처리전 : " + s + " 처리전 개수 : " + s.length());
+		System.out.print("처리후 : ");
+		System.out.print(ch);
+		System.out.println(" 처리후 개수 : " + ch.length);
+		System.out.println("대문자 변환 : " + s2);
+		
 	}
 
 }
